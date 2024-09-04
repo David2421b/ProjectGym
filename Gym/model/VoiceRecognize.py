b4d4076b9ceaedca2643825204e3350677ece4f2
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-09-04 12:55:54 -0500

    feat: Update voice recognition language to 'es-CO' in VoiceRecognize.py

--- Gym/model/VoiceRecognize.py
+++ Gym/model/VoiceRecognize.py
@@ -8,6 +8,8 @@ with mic as source:
 
     audio = recognizer.listen(source)
 
-text = recognizer.recognize_google(audio, language = 'ES')
+text = recognizer.recognize_google(audio, language = 'es-CO')
 
 print(f'Has dicho: {text}')
+print(type(audio)) #aca sabremos cual es el tipo de dato que se esta manejando
+
