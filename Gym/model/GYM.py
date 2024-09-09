dc9ec629d901292346dffd17975c696650c8fa1e
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-09-09 14:51:40 -0500

    chore: Add classes for User, Exercises, Routines, Medical History, Reminders, Reports, Notifications, and Statistics

--- Gym/model/GYM.py
+++ Gym/model/GYM.py
@@ -1,6 +1,46 @@
 #Todo el codigo va aca
 import ollama
 
+class Usuario:
+    
+     def __init__(self, nombre: str, edad: int, genero: str, peso: float, altura: float):
+        self.nombre: str = nombre
+        self.edad: int = edad
+        self.genero: str = genero
+        self.peso: float = peso
+        self.altura: float = altura
+    
+
+
+
+
+
+class Ejercicios:
+    
+    def __init__(self, tipo: str, repeticiones: int, series: int, descanzo: int):
+        self.tipo: str = tipo
+        self.repeticiones: int = repeticiones
+        self.series: int = series
+        self.descanzo: int = descanzo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # aca inicio el uso de Ollama
 def chatOllama(mensaje):
@@ -15,4 +55,4 @@ Mensaje = input("Si desea salir del chat solo escriba 'Salir' \n Cual es tu preg
 
 print(chatOllama(Mensaje))
 
-# FInaliza el uso de Ollama
+# FInaliza el uso de Ollamaa
