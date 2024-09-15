c7deda765cf70504d7e49a73035cc9ce09bee307
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-09-15 14:30:14 -0500

    Refactor code to update the order of parameters in the Usuario class constructor

--- Gym/model/GYM.py
+++ Gym/model/GYM.py
@@ -88,13 +88,13 @@ def main_2():           #Esta funcion inicia la ventana secundaria
 
 class Usuario:
     
-     def __init__(self, nombre: str, edad: int, email: str, genero: str, fecha_nacimiento: str, id_persona: str):
+     def __init__(self, nombre: str, id_persona: str, edad: int, email: str, genero: str, fecha_nacimiento: str):
         self.nombre: str = nombre
+        self.id_persona: str = id_persona
         self.edad: int = edad
         self.email: str = email
         self.genero: str = genero
         self.fecha_nacimiento: str = fecha_nacimiento
-        self.id_persona: str = id_persona
         
 
 class Rutinas:
