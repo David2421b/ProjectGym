162c49f8b39520004232382c3221db2e9044dbb3
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-09-15 00:08:17 -0500

    Refactor code to remove unnecessary imports, blank lines, and unused code in GYM.py and GymTkinter.py

--- Gym/model/GYM.py
+++ Gym/model/GYM.py
@@ -2,17 +2,21 @@
 import ollama
 from dataclasses import dataclass
 import tkinter as tk
-import 
 
 class Interfaz:
     
     def __init__(self, ventana):
         self.ventana = ventana
+
+class Interfaz_1 (Interfaz):
+
+    def __init__(self, ventana):
+        super().__init__(ventana)
         self.ventana.title("Chat de Meta")
         self.ventana.attributes('-fullscreen', True)
-        self.crea_widget()
-    
-    def crea_widget(self):
+        self.crea_widget_1()
+
+    def crea_widget_1(self):
         self.bienvenida = tk.Label(self.ventana, text = "Bienvenido al chat de Meta!!!", background = "gray", font = ("Arial", 25))
         self.bienvenida.pack(fill=tk.X)
 
@@ -25,7 +29,7 @@ class Interfaz:
         self.btn_salir.pack(side = tk.LEFT)
 
         # entrada de texto
-        self.entrada = tk.Entry(self.ventana, width=50)
+        self.entrada = tk.Entry(self.ventana, width = 50)
         self.entrada.pack()
 
         # boton para actvar el chat
@@ -35,11 +39,6 @@ class Interfaz:
         # boton para cambiar de interfaz
         self.btn_interfaz2 = tk.Button(self.frame_botones, text = "Interfaz 2", padx = 30, pady = 5, command = self.segunda_ventana)
         self.btn_interfaz2.pack(side = tk.RIGHT)
-        
-        # ------------------------------------------------ Segunda Interfaz ------------------------------------------------
-
-        def crear_widgets2(self):
-            pass
     
     def activar_chat(self):
         self.mensaje = self.entrada.get()
@@ -48,17 +47,37 @@ class Interfaz:
     def segunda_ventana(self):
         self.ventana.destroy()
         main_2()
+
+        
+class Interfaz_2 (Interfaz):
+        
+    def __init__(self, ventana):
+        super().__init__(ventana)
+        self.ventana.title("Segunda Interfaz")
+        self.ventana.attributes('-fullscreen', True)
+        self.crear_widgets_2()
+
+    def crear_widgets_2(self):
+        self.frame_botones = tk.Frame(self.ventana)
+        self.frame_botones.pack()
+
+        self.btn_salir = tk.Button(self.frame_botones, text = "Salir", padx = 30, pady = 5, command = self.ventana.destroy)
+        self.btn_salir.pack(side = tk.LEFT)
+
+    
+    
+    
     
 
 def main_1(): #Esta funcion inicia la ventana principal
     ventana_1 = tk.Tk()
-    app_1 = Interfaz(ventana_1)
+    app_1 = Interfaz_1(ventana_1)
     ventana_1.mainloop()
 
 
 def main_2():
     ventana_2 = tk.Tk()
-    app_2 = Interfaz(ventana_2)
+    app_2 = Interfaz_2(ventana_2)
     ventana_2.mainloop()
     
 
@@ -118,7 +137,7 @@ class Ollama:
             response = ollama.generate(model = 'llama3.1:latest', prompt = mensaje)
             return response['response']
 
-main_1() #con esta linea empezamos a ejecutar la interfaz
+main_1()#con esta linea empezamos a ejecutar la interfaz
 
 Mensaje = input("Si desea salir del chat solo escriba 'Salir' \n Cual es tu pregunta: ")
 
