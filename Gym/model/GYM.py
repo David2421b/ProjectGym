1aa7282c5c92053b9d13c0d96bd72e29308e1d1c
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-09-15 23:16:05 -0500

    Refactor code to update prompt message in GYM.py

--- Gym/model/GYM.py
+++ Gym/model/GYM.py
@@ -55,10 +55,10 @@ class Ollama:
             response = "has salido del chat, hasta la proxima"
             return response
         else:
-            response = ollama.generate(model = 'llama3.1:latest', prompt = mensaje)
+            response = ollama.generate(model = 'llama3.1:latest', prompt = mensaje)               
             return response['response']
 
 
-Mensaje = input("Si desea salir del chat solo escriba 'Salir' \n Cual es tu pregunta: ")
+Mensaje = input("\n Si desea salir del chat solo escriba 'Salir' \n Cual es tu pregunta: ")
 
 print(Ollama.chat(Mensaje))
