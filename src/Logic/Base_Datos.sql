7f95998e3e0efa0db6530d42ccfc67f3de1a9315
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-09-17 10:31:02 -0500

    Refactor code to remove unused files and update database schema

--- src/Logic/Base_Datos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    apellido TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    fecha_registro DATE DEFAULT (date('now'))
-);
---
+++ src/Logic/console.py
@@ -0,0 +1,1 @@
+from DigitalHealth import DigitalHealth
