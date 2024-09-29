4b552f2689bb15800d494d017cfdb28aae0d5b8c
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-09-29 17:36:17 -0500

    primera parte nicegui

---
+++ niceguitest.py
@@ -0,0 +1,42 @@
+from nicegui import ui
+from GYM.model.DigitalHealth import *
+from dataclasses import dataclass
+from GYM.Logic.DataBase import Database
+
+@dataclass
+class Interfaz_inicio:
+    #db: ClassVar[Database] = Database()
+    #db.conectar()
+    #menu(db)
+
+    def __post_init__(self):
+         self.menu()
+
+    def menu(self):
+        with ui.column().style("display:flex; flex-direction: column; align-items: center;"):
+                ui.label("Bienvenido a la aplicación de DigitalHealth").style("font-size: 40px; color: green; text-align: center;")
+                ui.label("------------------------------------------- MENÚ -------------------------------------------"). style("font-size: 20px; text-align: center; margin-top: 20px; margin-bottom: 20px;")
+                ui.label("opción '1' para Registrar nuevo usuario")
+                ui.label("opción '2' para Iniciar sesión")
+                ui.label("opción '3' para Salir \n")
+                opcion = ui.input("Selecciona una opción: ")
+
+        if opcion == "1":
+                self.registrar_usuario() #habia un db en los parentesis
+        elif opcion == "2":
+                self.iniciar_sesion() #habia un db en los parentesis
+        elif opcion == "3":
+            print("Saliendo del programa...")
+            #db.close()
+        else:
+            print("Opción no válida, intenta de nuevo.")
+    
+    def registrar_usuario(d):  #habia un db en los parentesis
+        pass
+
+    def iniciar_sesion():  #habia un db en los parentesis
+        pass
+
+
+interfaz = Interfaz_inicio()
+ui.run()
