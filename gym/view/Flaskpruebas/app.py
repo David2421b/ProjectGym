5356e44a7b1dbfc1f709951b6985cea85c7d2585
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-10-19 02:15:38 -0500

    Refactor margin in .back_login class in Index.html and update registration form in Registrarse.html

--- gym/view/Flaskpruebas/app.py
+++ gym/view/Flaskpruebas/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -21,11 +22,12 @@ def login():
         
         # Aquí puedes agregar la lógica para verificar el usuario (por ejemplo, comprobar en una base de datos)
         if username == 'admin' and password == 'admin':  # Ejemplo simple de verificación
-            return redirect(url_for('dashboard'))  # Redirige al dashboard si el inicio de sesión es exitoso
+            return redirect(url_for("dashboard"))  # Redirige al dashboard si el inicio de sesión es exitoso
         else:
             return "Usuario o contraseña incorrectos", 401  # Manejo de errores para credenciales incorrectas
 
     return render_template('login.html')  # Si no es un POST, muestra el formulario
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -33,6 +35,10 @@ def register():
         # Obtener los datos del formulario
         name = request.form['name']
 
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == '__main__':
