17bd56b4a9ef99d7b7d0600718088f07a1507035
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-10-19 02:34:22 -0500

    Refactor and delete unused files

--- gym/view/Flaskpruebas/app.py
+++ gym/view/Flaskpruebas/app.py
@@ -13,6 +13,11 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/menu')
+def menu():
+    return render_template("menu.html")
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -21,8 +26,8 @@ def login():
         password = request.form['password']  # Obtiene la contraseña
         
         # Aquí puedes agregar la lógica para verificar el usuario (por ejemplo, comprobar en una base de datos)
-        if username == 'admin' and password == 'admin':  # Ejemplo simple de verificación
-            return redirect(url_for("dashboard"))  # Redirige al dashboard si el inicio de sesión es exitoso
+        if username == 'a' and password == 'a':  # Ejemplo simple de verificación
+            return redirect(url_for("menu"))  # Redirige al menu si el inicio de sesión es exitoso
         else:
             return "Usuario o contraseña incorrectos", 401  # Manejo de errores para credenciales incorrectas
 
@@ -34,11 +39,6 @@ def register():
     if request.method == 'POST':
         # Obtener los datos del formulario
         name = request.form['name']
-
-
-@app.route('/dashboard')
-def dashboard():
-    return render_template("dashboard.html")
 
 
 if __name__ == '__main__':
